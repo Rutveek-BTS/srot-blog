99eb41ce9d773bda3101aeb953148b5c6f9dd9af
SAGE <rutveek21.rm@gmail.com>
2024-09-01 13:57:59 +0530

    app.js middlewares setup complete

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,23 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
 const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({
+    limit: '16kb' // This is the limit of how much json data can be accepted
+}))
+
+app.use(express.urlencoded({ // Used to receive data from URL
+    extended: true, // This handles the specials characters in URL like %20 for space, etc
+    limit: '16kb' // Max size of data that is acceptable from the URL
+}))
+
+app.use(cookieParser())
+
 export { app }
