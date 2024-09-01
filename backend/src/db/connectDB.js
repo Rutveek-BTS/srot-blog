c34e397a4c7ea4619f84481ece9d03c641c97064
SAGE <rutveek21.rm@gmail.com>
2024-09-01 13:10:52 +0530

    Database Connected

---
+++ backend/src/db/connectDB.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+import {DB_Name} from "../constants.js"
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`);
+    }catch(err){
+        console.log('ERROR: ', err)
+        process.exit(1)
+    }
+}
+
+export default connectDB;
