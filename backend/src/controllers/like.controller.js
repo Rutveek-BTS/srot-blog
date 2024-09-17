43edacf0733f214edeab72386df6dfbb6f685e84
SAGE <rutveek21.rm@gmail.com>
2024-09-17 14:11:32 +0530

    Like Controller Ready

--- backend/src/controllers/like.controller.js
+++ backend/src/controllers/like.controller.js
@@ -39,12 +39,71 @@ const toggleLike = asyncHandler( async (req, res)=>{
     .json(new apiResponse(200, newLike, "Like Blog Successful"))
 })
 
-const getUserBlogLikes = asyncHandler( async (req, res)=>{
-
-})
-
 const getBlogLikes = asyncHandler( async (req, res)=>{
-    
+    const {blogid} = req.params;
+
+    if(!isValidObjectId(blogid)){
+        throw new apiError(400, "Invalid User Id")
+    }
+
+    if(!req.user?._id){
+        throw new apiError(400, "No User Logged In")
+    }
+
+    const likes = await Like.aggregate([
+        {
+            $match: {
+                blog: new mongoose.Types.ObjectId(blogid)
+            }
+        },
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'likedBy',
+                foreignField: '_id',
+                as: 'liker',
+                pipeline: [
+                    {
+                        $project: {
+                            uName: 1,
+                            fName: 1,
+                            lName: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                liker: {
+                    $first: '$liker'
+                },
+                isLiked: {
+                    $cond: {
+                        if: {$eq: [new mongoose.Types.ObjectId(req.user._id), '$likedBy']},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                likedBy: 1,
+                liker: 1,
+                isLiked: 1
+            }
+        }
+    ])
+
+    if(!likes){
+        throw new apiError(500, "Something Went Wrong While Fetching The Likes")
+    }
+
+    return res
+    .status(200)
+    .json(new apiResponse(200, likes, "Likes Fetched Successfully"))
 })
 
-export {toggleLike, getUserBlogLikes, getBlogLikes}
+export {toggleLike, getBlogLikes}
--- backend/src/routes/like.route.js
+++ backend/src/routes/like.route.js
@@ -1,12 +1,11 @@
 import {Router} from 'express'
 import {verifyJWT} from '../middleware/auth.middleware.js'
-import {toggleLike, getUserBlogLikes, getBlogLikes} from "../controllers/like.controller.js";
+import {toggleLike, getBlogLikes} from "../controllers/like.controller.js";
 
 const router = Router();
 
 router.route('/:blogid').post(verifyJWT, toggleLike);
 
-router.route('/getuserbloglikes/:blogid').get(verifyJWT, getUserBlogLikes);
 router.route('/getbloglikes/:blogid').get(verifyJWT, getBlogLikes);
 
 export default router;
